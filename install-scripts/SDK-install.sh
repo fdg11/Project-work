6df5092b0f4583f46135e9470c7d9a63d1eea607
Alex P <alexfile123@gmail.com>
2017-08-29 16:52:10 +0300

    Update SDK-install.sh

--- install-scripts/SDK-install.sh
+++ install-scripts/SDK-install.sh
@@ -9,10 +9,10 @@ export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 
 # Import the Google Cloud Platform public key
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 
 # Update the package list and install the Cloud SDK
 apt-get update && apt-get install google-cloud-sdk -y 
 
-echo -e $(gcloud --version)
-
+echo -e "\n$(gcloud --version)"
+sleep 8
