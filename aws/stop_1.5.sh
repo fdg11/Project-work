cf4148b61f0afe0a1a01544f39e63ee83326bafb
Alex P <alexfile123@gmail.com>
2017-09-04 22:34:22 +0300

    update

---
+++ aws/stop_1.5.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Resetting previous actions 
+
+# Terminates instance
+INSTID=$(aws ec2 describe-instance-status --filters Name=instance-state-name,Values=running --query 'InstanceStatuses[*].InstanceId')
+
+j=0
+
+for i in $INSTID; do
+
+	aws ec2 terminate-instances --instance-ids $i --query 'TerminatingInstances[*].[CurrentState.Name]'
+	
+	while true; do	
+
+		STATE=$(aws ec2 describe-instances --instance-ids $i --query 'Reservations[*].Instances[*].[State.Name]')
+
+		if [ "$STATE" = "terminated" ]; then
+
+			let j++
+			break
+		
+		fi
+
+	done	
+
+done
+
+if [ "$j" = "3" ]; then
+
+	VPCID=(aws ec2 describe-vpcs --filter Name=tag:Name,Values=NAT --query 'Vpcs[*].VpcId')
+	
+	# Deleting a key pair
+        aws ec2 delete-key-pair --key-name $KEYNAME
+
+	# Releases the specified Elastic IP address
+ 	ALLOCID=$(aws ec2 describe-addresses --query 'Addresses[*].AllocationId')       
+	aws ec2 release-address --allocation-id $ALLOCID
+
+fi
+
+echo -e "\n Undo step 1.1–1.4"
+
+
+
